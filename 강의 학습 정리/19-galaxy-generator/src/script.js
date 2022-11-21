cdeb467dd18ace7b121d8fda121e8408a838dd6c
INSOCCI <5399686@naver.com>
2022-11-21 23:51:55 +0900

    Update 19강-debug gui

--- 강의 학습 정리/19-galaxy-generator/src/script.js
+++ 강의 학습 정리/19-galaxy-generator/src/script.js
@@ -9,7 +9,7 @@ import * as dat from "lil-gui";
  * Base
  */
 // Debug
-const gui = new dat.GUI();
+const gui = new dat.GUI(); //gui 추가
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -20,28 +20,48 @@ const scene = new THREE.Scene();
 /**
  * Galaxy
  */
-// 매번 새로 생성해주기 귀찮으니까~
+
+const parameters = {}; // 은하의 반지름이나 사이즈, 회전? 매개변수를 담을 객체 생성
+parameters.count = 10000;
+parameters.size = 0.01;
+parameters.radius = 5;
+parameters.branches = 3;
+
+// 매번 새로 생성해주어야 하므로, 아래 변수들은 함수 밖으로 꺼내준다.
 let geometry = null;
 let material = null;
 let points = null;
 
-const parameters = {}; // 은하의 반지름이나 사이즈, 회전? 매개변수를 담을 객체 생성
-parameters.count = 1000;
-parameters.size = 0.02;
-
 // 실행될 때마다 은하를 랜덤하게 생성해줄 함수
 const generateGalaxy = () => {
-  const geometry = new THREE.BufferGeometry();
-  const positions = new Float32Array(parameters.count * 3); //3을 해주는 이유 : 각 꼭지점이 X,Y,Z 3개의 값을 가져서
-  // 랜덤으로 위치 설정. -> 대신 겹치지 않게 차례대로 해줄 것임.
-  for (let i = 0; i < parameters.count; i++) {
-    const i3 = i * 3;
-
-    positions[i3 + 0] = Math.random(); // x 0 3 6
-    positions[i3 + 1] = Math.random(); // y 1 4 7
-    positions[i3 + 2] = Math.random(); // z 2 5 8
+  // Destroy old galaxy
+  if (points !== null) {
+    geometry.dispose();
+    material.dispose();
+    scene.remove(points);
   }
-  console.log(positions); // 간편하게 3000개의 배열이 생성된 것을 확인할 수 있음
+  geometry = new THREE.BufferGeometry();
+  const positions = new Float32Array(parameters.count * 3); // x,y,z 3개
+
+  for (let i = 0; i < parameters.count; i++) {
+    const i3 = i * 3; // 0 3 6 9 12 ...
+
+    // 은하 모양 속성
+    const radius = Math.random() * parameters.radius;
+    const spinAngle = radius * parameters.spin;
+    const branchAngle =
+      ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+
+    positions[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius;
+    positions[i3 + 1] = 0;
+    positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius;
+
+    //x, y, z 차례대로 겹치지 않게 랜덤한 위치값 설정
+    positions[i3 + 0] = (Math.random() - 0.5) * 3; // x 0 3 6
+    positions[i3 + 1] = (Math.random() - 0.5) * 3; // y 1 4 7
+    positions[i3 + 2] = (Math.random() - 0.5) * 3; // z 2 5 8
+  }
+  console.log(positions); // 콘솔창을 확인해보면 3000개의 위치값이 담긴 배열이 생성된 것을 확인 가능.
 
   geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
 
@@ -58,10 +78,39 @@ const generateGalaxy = () => {
   /**
    * Points
    */
-  const points = new THREE.Points(geometry, material);
+  points = new THREE.Points(geometry, material);
   scene.add(points);
 };
 generateGalaxy();
+
+//gui - count
+gui
+  .add(parameters, "count")
+  .min(100)
+  .max(1000000)
+  .step(100)
+  .onFinishChange(generateGalaxy); // onChange로 하면 드래그 클릭 시작한 순간부터 어마하게 은하 생성->컴터 힘드렁
+//gui - size
+gui
+  .add(parameters, "size")
+  .min(0.001)
+  .max(0.1)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+//gui - radius
+gui
+  .add(parameters, "radius")
+  .min(0.01)
+  .max(20)
+  .step(0.01)
+  .onFinishChange(generateGalaxy);
+  //gui - branches
+gui
+  .add(parameters, "branches")
+  .min(2)
+  .max(20)
+  .step(1)
+  .onFinishChange(generateGalaxy);
 
 /**
  * Sizes
